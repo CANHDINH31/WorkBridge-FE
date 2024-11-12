0eed2729d427bb9fc19909d7fa688faf4ba2ca3a
dinhphamcanh <dinhphamcanh@gmail.com>
2024-11-13 00:35:38 +0700

    feat: create theme

--- src/styles/theme/create-theme.ts
+++ src/styles/theme/create-theme.ts
@@ -1,7 +1,6 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
 
 import { colorSchemes } from './color-schemes';
-import { components } from './components/components';
 import { shadows } from './shadows';
 import type { Theme } from './types';
 import { typography } from './typography';
@@ -39,7 +38,30 @@ declare module '@mui/material/styles/createPalette' {
 export function createTheme(): Theme {
   const theme = extendTheme({
     breakpoints: { values: { xs: 0, sm: 600, md: 900, lg: 1200, xl: 1440 } },
-    components,
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          '@global': {
+            '*': {
+              boxSizing: 'border-box',
+              margin: '0',
+              padding: '0',
+            },
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: 'none',
+            borderRadius: 8,
+          },
+          outlined: {
+            backgroundColor: 'transparent',
+          },
+        },
+      },
+    },
     colorSchemes,
     shadows,
     shape: { borderRadius: 8 },
