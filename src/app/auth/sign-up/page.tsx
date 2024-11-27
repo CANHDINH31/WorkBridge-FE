cf85d2fcb8d08121ee4d110e926b7f00f5ab45b0
dinhphamcanh <dinhphamcanh@gmail.com>
2024-11-27 23:46:21 +0700

    feat: update sign up page

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -2,7 +2,25 @@ import * as React from 'react';
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import { EyeIcon } from '@heroicons/react/24/outline';
-import { Box, Button, Divider, InputAdornment, Paper, Stack, TextField, Typography } from '@mui/material';
+import { DatePicker } from '@mui/lab';
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 import { config } from '@/config';
 import { paths } from '@/paths';
@@ -23,7 +41,7 @@ export default function Page(): React.JSX.Element {
               <Typography fontSize={12}>Tận dụng tối đa cuộc sống nghề nghiệp của bạn.</Typography>
               <Box mt={4}>
                 <TextField fullWidth size="medium" placeholder="Name" />
-                <TextField fullWidth size="medium" placeholder="Email" sx={{ mt: 2 }} />
+                <TextField fullWidth size="medium" placeholder="Email" sx={{ mt: 1 }} />
                 <TextField
                   fullWidth
                   size="medium"
@@ -38,6 +56,29 @@ export default function Page(): React.JSX.Element {
                     ),
                   }}
                 />
+                <TextField fullWidth size="medium" placeholder="Phone" sx={{ mt: 1 }} />
+                <FormControl fullWidth sx={{ mt: 1 }}>
+                  <InputLabel id="role">Role</InputLabel>
+                  <Select
+                    labelId="role"
+                    id="demo-simple-select"
+                    // value={age}
+                    label="role"
+                    // onChange={handleChange}
+                  >
+                    <MenuItem value="1">Buyer</MenuItem>
+                    <MenuItem value="2">Seller</MenuItem>
+                    <MenuItem value="3">Broker</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <FormControl sx={{ mt: 1 }}>
+                  <RadioGroup row aria-labelledby="gender" name="gender">
+                    <FormControlLabel value="female" control={<Radio size="small" />} label="Female" />
+                    <FormControlLabel value="male" control={<Radio size="small" />} label="Male" />
+                    <FormControlLabel value="other" control={<Radio size="small" />} label="Other" />
+                  </RadioGroup>
+                </FormControl>
               </Box>
               <Typography fontSize={12} mt={2}>
                 Khi nhấp vào Đồng ý và tham gia hoặc Tiếp tục, bạn đồng ý với Thỏa thuận người dùng, Chính sách quyền
