813aff5cb4e3d11f485445155a4affa697808177
dinhphamcanh <dinhpc@solashi.com>
2024-12-04 13:28:08 +0700

    update: transaction ui

--- src/components/screens/transaction/transaction-page-view.tsx
+++ src/components/screens/transaction/transaction-page-view.tsx
@@ -1,20 +1,29 @@
 'use client';
 
-import { CURRENCY, DISCLOSURE, ITEM_CATEGORY, ROLE } from '@/utilities/contants';
+import {
+  CURRENCY,
+  DISCLOSURE,
+  ITEM_CATEGORY,
+  ITEM_CATEGORY_TYPE_1,
+  ITEM_CATEGORY_TYPE_2,
+  ItemCategory,
+  ROLE,
+  SHIPPING_METHOD,
+  SHIPPING_PAID_BY,
+} from '@/utilities/contants';
+import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
 import {
   Autocomplete,
   Box,
   Button,
   Divider,
   FormControl,
-  FormControlLabel,
   InputAdornment,
   InputLabel,
   MenuItem,
   OutlinedInput,
   Paper,
   Select,
-  Switch,
   TextField,
   Typography,
 } from '@mui/material';
@@ -33,10 +42,13 @@ function TransactionPageView(props: Props) {
   } = useForm();
 
   const role = watch('role') ?? ROLE[0].value;
-
   const currency = watch('currency') ?? CURRENCY[0].value;
+  const itemCategory: ItemCategory = watch('item_category') ?? ITEM_CATEGORY[0];
+  const shippingMethod = watch('shipping_method') ?? SHIPPING_METHOD[0]?.value;
+  const shippingFeePaidBy = watch('shipping_fee_paid_by') ?? SHIPPING_PAID_BY[0]?.value;
 
-  const itemCategory = watch('item_category') ?? ITEM_CATEGORY[0];
+  const showCategoryType1 = ITEM_CATEGORY_TYPE_1.map((item) => item.value).includes(itemCategory.value);
+  const showCategoryType2 = ITEM_CATEGORY_TYPE_2.map((item) => item.value).includes(itemCategory.value);
 
   return (
     <Box width="100%">
@@ -153,7 +165,12 @@ function TransactionPageView(props: Props) {
                   )}
                 />
               </Box>
-              {itemCategory !== ITEM_CATEGORY[0] && (
+
+              <Box mt={2}>
+                <TextField fullWidth label="Item description" multiline rows={3} />
+              </Box>
+
+              {showCategoryType2 ? (
                 <Box>
                   <Box mt={2} display="flex" gap={2}>
                     <TextField label="Make" fullWidth />
@@ -166,10 +183,77 @@ function TransactionPageView(props: Props) {
                   <Box mt={2}>
                     <TextField label="VIN" fullWidth />
                   </Box>
+                  <Box mt={2} display="flex" gap={2}>
+                    <FormControl fullWidth>
+                      <InputLabel id="shipping_method">Shipping Method</InputLabel>
+                      <Controller
+                        name="shipping_method"
+                        control={control}
+                        defaultValue={SHIPPING_METHOD[0].value}
+                        rules={{ required: 'Shipping method is required' }}
+                        render={({ field }) => (
+                          <Select
+                            labelId="shipping_method"
+                            id="shipping_method"
+                            label="Shipping method"
+                            {...field}
+                            error={Boolean(errors.type)}
+                          >
+                            {SHIPPING_METHOD.map((s) => (
+                              <MenuItem key={s.value} value={s.value}>
+                                {s.label}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        )}
+                      />
+                    </FormControl>
+                    {shippingMethod !== SHIPPING_METHOD[1].value && (
+                      <>
+                        <FormControl fullWidth>
+                          <InputLabel id="shipping_fee_paid_by">Shipping fee paid by</InputLabel>
+                          <Controller
+                            name="shipping_fee_paid_by"
+                            control={control}
+                            defaultValue={SHIPPING_PAID_BY[0].value}
+                            render={({ field }) => (
+                              <Select
+                                labelId="shipping_fee_paid_by"
+                                id="shipping_fee_paid_by"
+                                label="Shipping fee paid by"
+                                {...field}
+                                error={Boolean(errors.type)}
+                              >
+                                {SHIPPING_PAID_BY.map((s) => (
+                                  <MenuItem key={s.value} value={s.value}>
+                                    {s.label}
+                                  </MenuItem>
+                                ))}
+                              </Select>
+                            )}
+                          />
+                        </FormControl>
+                        {shippingFeePaidBy === SHIPPING_PAID_BY[0]?.value && (
+                          <FormControl fullWidth>
+                            <FormControl fullWidth>
+                              <InputLabel htmlFor="outlined-adornment-amount">
+                                Price ({currency === CURRENCY[0]?.value ? 'USD' : 'VND'})
+                              </InputLabel>
+                              <OutlinedInput
+                                label="Price (USD)"
+                                startAdornment={<InputAdornment position="start"> $</InputAdornment>}
+                                fullWidth
+                              />
+                            </FormControl>
+                          </FormControl>
+                        )}
+                      </>
+                    )}
+                  </Box>
                 </Box>
-              )}
+              ) : null}
 
-              <Box mt={2}>
+              {/* <Box mt={2}>
                 <FormControlLabel control={<Switch defaultChecked />} label="Set transaction items as milestones" />
               </Box>
               <Box mt={2} display="flex" gap={2}>
@@ -200,9 +284,27 @@ function TransactionPageView(props: Props) {
                     />
                   </FormControl>
                 </FormControl>
-              </Box>
-              <Box mt={4} textAlign="center">
-                <Button variant="contained">Add Item</Button>
+              </Box> */}
+              <Box mt={4} display="flex" justifyContent="flex-end" gap={2}>
+                {showCategoryType1 ? (
+                  <>
+                    <Button color="success" size="large">
+                      Download csv template
+                    </Button>
+                    <Button
+                      color="success"
+                      size="large"
+                      variant="outlined"
+                      endIcon={<DocumentArrowDownIcon height={24} />}
+                    >
+                      Bulk upload by csv
+                    </Button>
+                  </>
+                ) : null}
+
+                <Button variant="contained" size="large">
+                  Add Item
+                </Button>
               </Box>
             </Box>
           </Box>
--- src/utilities/contants.ts
+++ src/utilities/contants.ts
@@ -1,3 +1,8 @@
+export interface ItemCategory {
+  label: string;
+  value: string;
+}
+
 export const TYPE = [
   {
     label: 'Seller',
@@ -29,6 +34,17 @@ export const ROLE = [
   {
     label: 'Broker',
     value: 'broker',
+  },
+];
+
+export const SHIPPING_PAID_BY = [
+  {
+    label: 'Buyer',
+    value: 'buyer',
+  },
+  {
+    label: 'Seller',
+    value: 'seller',
   },
 ];
 
@@ -68,9 +84,63 @@ export const GENDER = [
   { label: 'Other', value: 'other' },
 ];
 
-export const ITEM_CATEGORY = [
+export const ITEM_CATEGORY_TYPE_1 = [
   { label: 'Domain', value: 'domain' },
   { label: 'Website', value: 'website' },
+];
+
+export const ITEM_CATEGORY_TYPE_2 = [
+  {
+    label: 'Cars & Trucks',
+    value: 'cars_and_trucks',
+  },
   { label: 'Boats', value: 'boats' },
+  { label: 'Motorcycles', value: 'motorcycles' },
   { label: 'Airplanes', value: 'airplanes' },
+  { label: 'Other Motor Vehicles', value: 'other_moto_vehicles' },
+];
+
+export const ITEM_CATEGORY_TYPE_3 = [
+  {
+    label: 'Antiques & Collectibles',
+    value: 'antiques_and_collecibles',
+  },
+  {
+    label: 'Appliances',
+    value: 'appliances',
+  },
+  {
+    label: 'Arts & Crafts',
+    value: 'arts_and_crafts',
+  },
+  { label: 'Automotive or Industrial Parts', value: 'automotiv_or_industrial_parts' },
+  { label: 'Beauty & Health', value: 'beauty_and_health' },
+  { label: 'Books & Magazines', value: 'books_and_magazines' },
+  { label: 'Clothing & Accessories', value: 'clothing_and_accessories' },
+  { label: 'Computer Hardware & Software', value: 'computer_hardware_and_software' },
+  { label: 'Cosmetic Injectables', value: 'cosmetic_injectables' },
+  { label: 'Electronics', value: 'electronics' },
+  { label: 'Food', value: 'food' },
+  { label: 'Furniture', value: 'furniture' },
+  { label: 'Heavy Equipment & Machinery', value: 'heavy_quipment_and_machinery' },
+  { label: 'Intellectual Property', value: 'intellectual_and_property' },
+  { label: 'Ip Addresses', value: 'ip_addresses' },
+  { label: 'Jewellery & Watches', value: 'jewellery_and_watches' },
+  { label: 'Mobile Apps', value: 'mobile_apps' },
+  { label: 'Movies & Music', value: 'movies_and_music' },
+  { label: 'Pets & Livestock', value: 'pets_and_livestock' },
+  { label: 'Services', value: 'services' },
+  { label: 'Social Media Accounts', value: 'social_media_accounts' },
+  { label: 'Sports & Recreation', value: 'sports_and_recreation' },
+  { label: 'Tickets & Events', value: 'tickets_and_events' },
+  { label: 'Tools & Hardware', value: 'tools_and_hardware' },
+  { label: 'Toys & Hobbies', value: 'toys_and_hobbies' },
+  { label: 'Video Games & Consoles', value: 'video_game_and_consoles' },
+];
+
+export const ITEM_CATEGORY = ITEM_CATEGORY_TYPE_1.concat(ITEM_CATEGORY_TYPE_2).concat(ITEM_CATEGORY_TYPE_3);
+
+export const SHIPPING_METHOD = [
+  { label: 'Cargo Shipping', value: 'cargo_shipping' },
+  { label: 'No Shipping', value: 'no_shipping' },
 ];
